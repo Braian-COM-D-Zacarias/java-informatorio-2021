9b81cb77e5585460ae4e519cbebeae365608cdb2
Braian-COM-D-Zacarias <63023211+Braian-COM-D-Zacarias@users.noreply.github.com>
2021-10-13 21:33:56 -0300

    Update Ejercicio3.java

--- Complementarios-2/Ejercicio3.java
+++ Complementarios-2/Ejercicio3.java
@@ -1,8 +1,8 @@
-
 import java.util.Arrays;
 import java.util.Random;
 
 public class Ejercicio3 {
+    
     public static void main(String[] args){
         int [] baraja = {2,3,4,5,6,7,8,9,10};
         System.out.println("Baraja ordenadas: " + Arrays.toString(baraja));
