b28e86770d05c00a89650be19b4454c6b048f818
Braian-COM-D-Zacarias <63023211+Braian-COM-D-Zacarias@users.noreply.github.com>
2021-10-13 21:39:20 -0300

    Update Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+
 public class Ejercicio5 {
+	
 	 public static void main(String[]args) {
+		 
 		 Scanner scan = new Scanner(System.in);      //Creamos un scanner que tomara los valores de entrada por consola
 		 int nro = scan.nextInt();        //Escanea el proximo entero que es ingresado
 		 scan.close();                            //Ceeramos el scanner si no lo usamos mas(buena practica)
