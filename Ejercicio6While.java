7f10710cc6fd108b254ec90e0b945ae4b8e36d06
Braian-COM-D-Zacarias <63023211+Braian-COM-D-Zacarias@users.noreply.github.com>
2021-10-13 21:40:38 -0300

    Update Ejercicio6While.java

--- Ejercicio6While.java
+++ Ejercicio6While.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+
 public class Ejercicio6While {
 	 public static void main(String[]args) {
-		 Scanner scan = new Scanner(System.in);      //Creamos un scanner que tomara los valores de entrada por consola
-         int continuar = 1;
-         int nro ;
-         do{
+	   Scanner scan = new Scanner(System.in);      //Creamos un scanner que tomara los valores de entrada por consola
+           int continuar = 1;
+           int nro ;
+           do{
         	 System.out.println("Por favor ingrese un numero: ");
         	 nro = scan.nextInt();     //Escanea el primer numero ingresado
         	 System.out.println("El numero ingresa es: " + nro);
@@ -12,7 +13,7 @@ public class Ejercicio6While {
         	 System.out.println("Si desea continuar ingrese el numero 1, si no cualquiero otra tecla");
         	 continuar = scan.nextInt();     //Escanea el numero y evalua si se continua o no
         	 
-         } while (continuar == 1);
-         scan.close();     //Se cierra el escaner si no se usa mas
+            } while (continuar == 1);
+           scan.close();     //Se cierra el escaner si no se usa mas
 	 }
 }
