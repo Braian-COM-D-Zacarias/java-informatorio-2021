0203204124f97ad4696683c1c19788da72564ff1
Braian-COM-D-Zacarias <63023211+Braian-COM-D-Zacarias@users.noreply.github.com>
2021-10-13 21:22:51 -0300

    Update Ejercicio1.java

--- Complementarios-1/Ejercicio1.java
+++ Complementarios-1/Ejercicio1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Ejercicio1 {
+    
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         System.out.print("Nombre: ");
