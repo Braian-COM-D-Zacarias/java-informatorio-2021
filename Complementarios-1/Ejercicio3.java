54ab4cc520c7ef5ff527c7fc108da01a0f99c2ac
Braian-COM-D-Zacarias <63023211+Braian-COM-D-Zacarias@users.noreply.github.com>
2021-10-13 21:27:07 -0300

    Update Ejercicio3.java

--- Complementarios-1/Ejercicio3.java
+++ Complementarios-1/Ejercicio3.java
@@ -5,7 +5,6 @@ public class Ejercicio3 {
         int a = ingresarNumero();
         semitriangulo(a);
     }
-
     public static int ingresarNumero(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Ingrese un numero: ");
@@ -13,14 +12,12 @@ public class Ejercicio3 {
         scan.close();
         return x;
     }
-
     public static void semitriangulo(int x){
         for (int n=1; n<=x; n++){
             linea(n);
             System.out.println("");
         }
     }
-
     public static void linea(int x){
         for (int n=1; n<=x; n++){
             System.out.print(n + " ");
